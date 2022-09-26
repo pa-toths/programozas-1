8841c7e58a89e1ba1bfb481bbff6c1c8da47023f
pa-feketeb <112852275+pa-feketeb@users.noreply.github.com>
2022-09-26 08:16:38 +0200

    Create programozas.py

---
+++ programozas.py
@@ -0,0 +1,18 @@
+x = 5
+y = "John"
+print(x)
+print(type(x))
+print(y)
+print(type(y))
+
+x = 6.2
+print(x)
+print(type(x))
+
+x = 4
+print(x)
+print(type(x))
+
+x = "Sally"
+print(x)
+print(type(x))
