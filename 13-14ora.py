64b4249ce705da867f679b4e67e24c6b12efe51e
pa-feketeb <112852275+pa-feketeb@users.noreply.github.com>
2022-10-15 09:13:19 +0200

    Create 13-14ora.py

---
+++ 13-14ora.py
@@ -0,0 +1,18 @@
+# első feladat be kérek 2 számot nagyobbat írassuk ki 
+egyikszam=int(input('kérem az első számot:'))
+masikkszam=int(input('kérem az második számot:'))
+if egyikszam>masikkszam:
+    print(egyikszam)
+elif masikkszam>egyikszam:
+    print(masikkszam)
+else: 
+    print('egyikszam megegyezik a másikal')
+
+
+
+"""
+if egyikszam > masikkszam :
+    print(egyikszam)
+if masikkszam > egyikszam :
+    print(masikkszam)
+"""
