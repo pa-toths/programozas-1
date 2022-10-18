13da29adc28b50874964a35c2668af93c88dda69
pa-feketeb <112852275+pa-feketeb@users.noreply.github.com>
2022-10-18 13:12:03 +0200

    Create 13-14-hf.py

---
+++ 13-14-hf.py
@@ -0,0 +1,13 @@
+jegy = input('kérek egy osztályzatott: ')
+if jegy == "1":
+    print('elégtelen')
+elif jegy == "2":
+    print('elégséges')
+elif jegy == "3":
+    print('közepes')
+elif jegy == "4":
+    print('jó')
+elif jegy == "5":
+    print('jeles')
+else:
+    print("nem érvényes jegyet adtál meg")
