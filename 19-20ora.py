29e138c0a144162c19b5f4464bfc69b56a6cde6b
pa-feketeb <112852275+pa-feketeb@users.noreply.github.com>
2022-11-14 08:53:38 +0100

    Create 19-20ora.py

---
+++ 19-20ora.py
@@ -0,0 +1,55 @@
+"""
+a = "Hello, World!"
+
+print(a.upper())
+"""
+
+
+"""
+a = "Hello, World!"
+
+print(a.lower())
+"""
+
+
+"""
+a = " Hello, World! "
+
+print(a.strip()) # returns "Hello, World!"
+"""
+
+
+"""
+a = "HelHloHHelHloH, World!"
+print(a)
+uj_string = a.replace("HelHloH" , "Jo", )
+print("a régi string" + " " + a)
+print( "a régi string",a, sep= " ")
+print("az új " + uj_string)
+"""
+
+
+"""
+a = "Hello"
+b = "World"
+space = " "
+felkialto = "!"
+c = a + space + b + felkialto + space + a + space + b + felkialto
+print(c)
+"""
+"""
+quantity = 3
+
+itemno = 567
+
+price = 49.95
+
+myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
+
+print(myorder.format(quantity, itemno, price))
+"""
+
+print("10.D osztály 1 csoport", end="\n")
+print('\n')
+print("10.D osztály 1 csoport", end="\t")
+print("10.D osztály 1 csoport")
