7b4505eccf480a2ebc2b48a642c3ef00f442fcdd
pa-feketeb <112852275+pa-feketeb@users.noreply.github.com>
2022-11-14 09:41:48 +0100

    Create 21-22ora.py

---
+++ 21-22ora.py
@@ -0,0 +1,31 @@
+"""
+szam = 0
+while szam != 100:
+    szam = szam + 1 
+    print(szam)
+print("a program vége!")
+
+szam = 1
+while szam != 101:
+    print(szam)
+    szam = szam + 1 
+print("a program vége!")
+
+szam = 1
+while True:
+    print(szam)
+    szam = szam + 1 
+print("a program vége!")
+
+szam = 0
+while szam < 100:
+    szam = szam + 1 
+    print(szam)
+print("a program vége!")
+"""
+
+szam = 0
+while szam != 100:
+    szam = szam + 1 
+    print(szam)
+print("a program vége!")
