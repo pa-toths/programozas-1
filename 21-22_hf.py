8868f7bab500808ead0ac8fab32118132c66899c
pa-feketeb <112852275+pa-feketeb@users.noreply.github.com>
2022-11-20 23:22:31 +0100

    Create 21-22_hf.py

---
+++ 21-22_hf.py
@@ -0,0 +1,36 @@
+"""
+szam=0
+while szam<102:
+    print(szam)
+    szam=szam+2
+print("vége")
+
+
+
+szam=0
+while szam<102:
+    print(szam)
+    szam=szam+3
+print("vége")
+
+
+
+szam=0
+while szam<100:
+    if szam%2==1:
+        print(szam)
+    szam=szam+1
+print("vége")
+"""
+szam=0
+while szam<100:
+    if szam%2==0:
+        print(szam)
+    szam=szam+1
+print("vége")
+
+
+valami=True
+while valami:
+    print("házi feladat")
+    valami=Fals
