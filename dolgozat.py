c8f59a5d174efc7ebcfb28d210c92962785171a9
pa-feketeb <112852275+pa-feketeb@users.noreply.github.com>
2022-11-28 08:43:55 +0100

    Create dolgozat.py

---
+++ dolgozat.py
@@ -0,0 +1,40 @@
+"""
+szam=0
+for _ in range(100):
+    szam=szam + 1 
+    print(szam,end=" ")
+print()
+
+
+szam=1
+for _ in range (100):
+    print(szam,end=" ")
+    szam=szam +1
+print()
+
+
+szam=0
+for _ in range(1,101):
+    szam=szam +1
+    print(szam,end=" ")
+print()
+
+
+szam=0
+for _ in range(1,101,1):
+    szam=szam +1
+    print(szam,end=" ")
+print()
+
+
+szam=0
+for _ in range(5,105,1):
+    szam=szam +1 
+    print(szam,end="")
+print()
+"""
+
+
+for szam in range(100):
+    print(szam +1,end=" ")
+print()
