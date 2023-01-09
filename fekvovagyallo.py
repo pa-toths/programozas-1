ece667296ccdda719320c1b0d682be33a2d7ed73
pa-feketeb <112852275+pa-feketeb@users.noreply.github.com>
2023-01-09 09:21:49 +0100

    Update fekvovagyallo.py

--- fekvovagyallo.py
+++ fekvovagyallo.py
@@ -1,8 +1,9 @@
 szél = int(input("Adja meg a téglalap szélességét:"))
 mag = int(input("Adja meg a téglalap magasságát:"))
+T = szél * mag
 if mag > szél:
-    print("Ez egy álló téglalap és a területe" , mag * szél)
+    print("Ez egy álló téglalap és a területe" , T)
 elif szél > mag:
-    print("Ez egy fekvő téglalap és a területe" , szél * mag)
+    print("Ez egy fekvő téglalap és a területe" , T)
 elif szél == mag:
-    print("Ez egy négyzet és a területe" , szél * mag)
+    print("Ez egy négyzet és a területe" , T)
