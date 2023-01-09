6e48139de84337204fae6fc042a8abcd2bc2dbb1
pa-feketeb <112852275+pa-feketeb@users.noreply.github.com>
2023-01-09 08:51:27 +0100

    Update eletszakaszok.py

--- eletszakaszok.py
+++ eletszakaszok.py
@@ -1,16 +1,16 @@
- név = input("Kérem a teljes nevét:")
+név = input("Kérem a teljes nevét:")
 év = int(input("Kérem az élet korát:"))
 if év < 2:
-    print("Magga még csecsemő")
+    print(név , "Magga még csecsemő")
 elif év < 5:
-    print("Magga még kisgyerek")
+    print(név , "Magga még kisgyerek")
 elif év < 12:
-    print("Magga még gyerek")
+    print(név , "Magga még gyerek")
 elif év < 16:
-    print("Magga még serdülő")
+    print(név , "Magga még serdülő")
 elif év < 25:
-    print("Magga már ifjú")
+    print(név , "Magga már ifjú")
 elif év < 65:
-    print("Magga már felnőtt")
+    print(név , "Magga már felnőtt")
 elif év > 65:
-    print("Magga már nyugdíjas")
+    print(név , "Magga már nyugdíjas")
