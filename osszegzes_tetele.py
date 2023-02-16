15ec0e6d5436e7dfe0c46fd2f341158d2a2b5002
pa-feketeb <112852275+pa-feketeb@users.noreply.github.com>
2023-02-16 08:20:23 +0100

    Create osszegzes_tetele.py

---
+++ osszegzes_tetele.py
@@ -0,0 +1,10 @@
+lista1 = [3, 12, 3, 4, 7, 15, 1]
+
+def oszzeges_tetele(lista_oszegzes):
+    osszesen = 0
+    for szam in lista_oszegzes:
+            osszesen = osszesen + szam
+
+    print("A listában található összege: ", osszesen)
+
+oszzeges_tetele(lista1)
