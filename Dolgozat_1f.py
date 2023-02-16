e719745a5bb83f5041ce7df3ac31cea17af94ee1
pa-feketeb <112852275+pa-feketeb@users.noreply.github.com>
2023-02-16 07:51:27 +0100

    Create Dolgozat_1f.py

---
+++ Dolgozat_1f.py
@@ -0,0 +1,10 @@
+#Fekete Balázs
+#10D 1Csoport
+
+szam = int(input("Adj meg egy számot! "))
+if szam < 0:
+    print('A megadott szám negatív.')
+elif szam == 0:
+    print("A megadott szám nulla.")
+else:
+    print("A megadott szám pozitív.")
